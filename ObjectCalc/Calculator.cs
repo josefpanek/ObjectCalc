71e3de88a80a67618c5e70ca7d53195e44d4cccd
Pepa Pánek <josef.panek@gmail.com>
2022-05-06 15:09:03 +0200

    Add project files.

---
+++ ObjectCalc/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectCalc
+{
+    internal class Calculator
+    {
+        public double Secti(double cislo1, double cislo2)
+        {
+            return cislo1 + cislo2;
+        }
+
+        public double Odecti(double cislo1, double cislo2)
+        {
+            return cislo1 - cislo2;
+        }
+        public double Vynasob(double cislo1, double cislo2)
+        {
+            return cislo1 * cislo2;
+        }
+        public double Vydel(double cislo1, double cislo2)
+        {
+            return cislo1 / cislo2;
+        }
+    }
+}
---
+++ ObjectCalc/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej 1. číslo: ");
+            double cislo1 = double.Parse(Console.ReadLine());
+            
+            Console.Write("Zadej 2. číslo: ");
+            double cislo2 = double.Parse(Console.ReadLine());
+
+            Calculator calculator = new Calculator();
+
+            Console.WriteLine("Součet: " + calculator.Secti(cislo1, cislo2));
+            Console.WriteLine("Rozdíl: " + calculator.Odecti(cislo1, cislo2));
+            Console.WriteLine("Součin: " + calculator.Vynasob(cislo1, cislo2));
+            Console.WriteLine("Podíl : " + calculator.Vydel(cislo1, cislo2));
+            Console.ReadKey();
+        }
+    }
+}
